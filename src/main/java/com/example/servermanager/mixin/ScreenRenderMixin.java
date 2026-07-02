7f7b42be87304165fe18ad92c1a3c6706bca421f
ayanishapgaming-create <ayanishapgaming@gmail.com>
2026-07-02 15:40:44 +0500

    Add files via upload

--- src/main/java/com/example/servermanager/mixin/ScreenRenderMixin.java
+++ src/main/java/com/example/servermanager/mixin/ScreenRenderMixin.java
@@ -9,7 +9,6 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(Screen.class)
 public abstract class ScreenRenderMixin {
@@ -20,32 +19,6 @@ public abstract class ScreenRenderMixin {
         TextFieldWidget field = SearchFieldRegistry.get((MultiplayerScreen) (Object) this);
         if (field != null) {
             field.render(context, mouseX, mouseY, delta);
-        }
-    }
-
-    @Inject(method = "mouseClicked", at = @At("HEAD"), cancellable = true)
-    private void onMouseClicked(double mouseX, double mouseY, int button, CallbackInfoReturnable<Boolean> cir) {
-        if (!((Object) this instanceof MultiplayerScreen)) return;
-        TextFieldWidget field = SearchFieldRegistry.get((MultiplayerScreen) (Object) this);
-        if (field == null) return;
-        boolean inBounds = mouseX >= field.getX() && mouseX <= field.getX() + field.getWidth()
-                        && mouseY >= field.getY() && mouseY <= field.getY() + field.getHeight();
-        if (inBounds) {
-            field.setFocused(true);
-            ((Screen) (Object) this).setFocused(field);
-            cir.setReturnValue(true);
-        } else {
-            field.setFocused(false);
-        }
-    }
-
-    @Inject(method = "keyPressed", at = @At("HEAD"))
-    private void onKeyPressed(int keyCode, int scanCode, int modifiers, CallbackInfoReturnable<Boolean> cir) {
-        if (!((Object) this instanceof MultiplayerScreen)) return;
-        TextFieldWidget field = SearchFieldRegistry.get((MultiplayerScreen) (Object) this);
-        if (field != null && field.isFocused() && keyCode == 256) {
-            field.setFocused(false);
-            ((Screen) (Object) this).setFocused(null);
         }
     }
 }
